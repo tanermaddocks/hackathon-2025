826e7d3a8deca22bbb3248ddebb36810adb6a738
tanermaddocks <maddockstaner@gmail.com>
2025-02-10 09:50:40 +1000

    Implemented update/patch operation for book entity

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -35,10 +35,35 @@ router.post(
     }
 )
 
+router.patch(
+    "/:bookId",
+    async (request, response) => {
+        const bodyData = {
+            title: request.body.title,
+            fiction: request.body.fiction,
+            genres: request.body.genres,
+            author: request.body.author,
+            rating: request.body.rating
+        }
+        const updatedBook = await BookModel.findByIdAndUpdate(
+            request.params.bookId, bodyData, { new: true}
+        );
+        if (!updatedBook) {
+            response.status(404).json({error: "Book not found"})
+        } else if (updatedBook.error) {
+            response.status(403).json(updatedBook)
+        } else { 
+            response.json(updatedBook)
+        }
+    }
+)
+
 router.delete(
     "/delete/:bookId",
     async (request, response) => {
-        const deletedBook = await BookModel.findByIdAndDelete(request.params.bookId)
+        const deletedBook = await BookModel.findByIdAndDelete(
+            request.params.bookId
+        );
         if (deletedBook) {
             response.json(deletedBook)
             console.log("Book deleted")
